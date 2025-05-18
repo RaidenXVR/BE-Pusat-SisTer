607c77645f1941d57c7a1081e6a54250ed551e1b
RaidenXVR <fitran.nizar@gmail.com>
2025-05-18 12:01:44 +0700

    moved DB connection to env

--- src/db.js
+++ src/db.js
@@ -1,9 +1,14 @@
 import mysql from 'mysql2/promise';
+import { configDotenv } from 'dotenv';
+
+configDotenv()
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 // MySQL connection pool (adjust credentials as needed)
 export const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'thepassword',
-    database: 'cabang_pusat'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 });
